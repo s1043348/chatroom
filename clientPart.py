009dc9823aff6b8247175b42065af65c77267e9a
MFPKChenPo <38970886+MFPKChenPo@users.noreply.github.com>
2018-05-25 15:27:05 +0800

    *新增*--使用者未輸入name前不能點擊send, 使用者輸入的文字顯示在右側

--- clientPart.py
+++ clientPart.py
@@ -8,12 +8,24 @@ class Main(QMainWindow, example_ui.Ui_MainWindow):
         super(self.__class__, self).__init__()
         self.setupUi(self)
         self.pushButton.setText("Send")
+        self.pushButton.setEnabled(False)
         self.pushButton_2.clicked.connect(self.onClick)
-
+        self.pushButton.clicked.connect(self.send)
 
     def onClick(self):
+        global userID
+        userID = self.lineEdit_2.text()
         self.lineEdit_2.setEnabled(False)
         self.pushButton_2.setEnabled(False)
+        self.pushButton.setEnabled(True)
+
+    def send(self):
+        text = self.lineEdit.text()
+        text = text + ' : ' + userID
+        text = "{: >70}".format(text)
+        self.textBrowser.append(text)
+        self.textBrowser.update()
+        self.lineEdit.setText("")
 
 
 
