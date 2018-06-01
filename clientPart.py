912cb8647affee0503b51f94633408c23a886284
MFPKChenPo <38970886+MFPKChenPo@users.noreply.github.com>
2018-06-01 15:49:23 +0800

    *update* 更新至可連線進行多方聊天

--- clientPart.py
+++ clientPart.py
@@ -1,39 +1,65 @@
 from PyQt5.QtWidgets import QMainWindow, QApplication
-import example_ui
+import main_window
 import sys
+import threading
+import socket
+class Main(QMainWindow, main_window.Ui_MainWindow):
 
-class Main(QMainWindow, example_ui.Ui_MainWindow):
-
-    def __init__(self):
+    def __init__(self, host, port):
         super(self.__class__, self).__init__()
         self.setupUi(self)
-        self.pushButton.setText("Send")
-        self.pushButton.setEnabled(False)
-        self.pushButton_2.clicked.connect(self.onClick)
-        self.pushButton.clicked.connect(self.send)
+        socktemp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)    ##by ChenPo
+        self.sock = socktemp    ##by ChenPo
+        self.sock.connect((host, port)) ##by ChenPo
+        self.sock.send(b'1')    ##by ChenPo
+        self.pushButton.setText("Send") ##by ChenPo
+        self.pushButton.setEnabled(False)#by ChenPo
+        self.pushButton_2.clicked.connect(self.onClick)#by ChenPo
+        self.pushButton.clicked.connect(self.send)#by ChenPo
 
     def onClick(self):
         global userID
         userID = self.lineEdit_2.text()
-        welcome = "Welcome to Chat Room ! " + userID
-        self.textBrowser.append(welcome)
+        self.sock.send(userID.encode())
         self.textBrowser.update()
         self.lineEdit_2.setEnabled(False)
         self.pushButton_2.setEnabled(False)
         self.pushButton.setEnabled(True)
+        th2 = threading.Thread(target=self.recv)#by ChenPo
+        th2.setDaemon(True)#by ChenPo
+        th2.start()#by ChenPo
+
+    def recv(self):#by ChenPo
+        while True:
+            otherword = self.sock.recv(1024)
+            self.textBrowser.append(otherword.decode())
+            self.textBrowser.update()
 
     def send(self):
-        text = self.lineEdit.text()
-        text = text + ' : ' + userID
-        text = "{: >70}".format(text)
-        self.textBrowser.append(text)
-        self.textBrowser.update()
-        self.lineEdit.setText("")
+        text = self.lineEdit.text()#by ChenPo
+        self.sock.send(text.encode())   ##by ChenPo
+        text = text + ' : ' + userID#by ChenPo
+        text = "{: >70}".format(text)#by ChenPo
+        self.textBrowser.append(text)#by ChenPo
+        self.textBrowser.update()#by ChenPo
+        self.lineEdit.setText("")#by ChenPo
 
+
+
+def main():#by ChenPo
+    user = Main('localhost', 5550)
+    th1 = threading.Thread(target=user.send)
+    th2 = threading.Thread(target=user.recv)
+    threads = [th1, th2]
+    for i in threads:
+        i.setDaemon(True)
+        i.start()
+    i.join()
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    MainWindow = Main()
+    MainWindow = Main('localhost', 5550)
     MainWindow.show()
     sys.exit(app.exec_())
+    main()#by ChenPo
