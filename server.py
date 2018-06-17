b3be3fbe6ddd8614bdf1e7e208073ee383a845b0
MFPKChenPo <38970886+MFPKChenPo@users.noreply.github.com>
2018-06-17 23:03:11 +0800

    *update* server端可以正常使用介面+讓client端正常通訊
    *刪除功能還沒新增，因為serverUI的列表還不能顯示出各個client，也就不能勾選起來刪除

--- server.py
+++ server.py
@@ -5,11 +5,10 @@ import datetime
 import sys
 from pymongo import MongoClient
 from bson.objectid import ObjectId
-import windowserver
+import serverwindow_ui
 from PyQt5.QtWidgets import QMainWindow, QApplication
 from time import gmtime, strftime
-global count
-global id
+
 
 class DataBaseChatRoom:
     def __init__(self):
@@ -24,22 +23,25 @@ class DataBaseChatRoom:
     # delete user by uname
     # dbChatRoom.deleteUser(['A'])
     def deleteUser(self, unameList):
-        self.collection.remove({'uname':unameList})
+        self.collection.remove({'uname': unameList})
 
     # insert user
     # dbChatRoom.insertUser(uname='A', upwd='A')
     def insertUser(self, uname, upwd):
-        self.collection.insert_one({'uname':uname,'upwd':upwd})
-
+        self.collection.insert_one({'uname': uname, 'upwd': upwd})
+        print("Server: user added!!")
 
     def updataUser(self, uname=None, upwd=None):
         pass
         return 'successful'
 
     # check checkUserExist
-    def checkUserExist(self, uname='A'):
-        pass
-        return False
+    def checkUserExist(self, uname):
+        temp = self.collection.find({'uname': uname})
+        if temp:
+            return True
+        else:
+            return False
 
     # query user bu uname
     # dbChatRoom.queryByuname(uname='A', upwd='A')
@@ -50,11 +52,21 @@ class DataBaseChatRoom:
     # Init database
     # dbChatRoom.Initdatabase()
     def Initdatabase(self):
-        pass
+        self.collection.delete_many({})
+        userList = []
+        userList.append({'uname': 'A', 'upwd': 'A'})
+        userList.append({'uname': 'B', 'upwd': 'B'})
+        userList.append({'uname': 'C', 'upwd': 'C'})
+        userList.append({'uname': 'D', 'upwd': 'D'})
+        userList.append({'uname': 'E', 'upwd': 'E'})
+        self.collection.insert_many(userList)
+        print("Server: initialize database!!")
 
     def colseClient(self):
         self.client.close()
-class Server(QMainWindow, windowserver.Ui_MainWindow):
+
+
+class Server(QMainWindow, serverwindow_ui.Ui_MainWindow):
     def __init__(self, host, port):
         super(self.__class__, self).__init__()
         self.setupUi(self)
@@ -63,17 +75,18 @@ class Server(QMainWindow, windowserver.Ui_MainWindow):
         self.sock.bind((host, port))
         self.sock.listen(5)
         print('Server', socket.gethostbyname(host), 'listening ...')
+        dbChatRoom = DataBaseChatRoom()
+        dbChatRoom.Initdatabase()   #reset database 剩下user A, B, C, D, E
+        self.count = 0
         self.mylist = list()
-        self.namelist = list()
         self.pushButton.clicked.connect(self.Input)
+        self.pushButton_2.clicked.connect(self.DelUser)
 
     def checkConnection(self):
-        global count
         connection, addr = self.sock.accept()
-        count += 1
-        print(count)
+        self.count += 1
+        print(self.count)
         print('Accept a new connection', connection.getsockname(), connection.fileno())
-
 
         try:
             buf = connection.recv(1024).decode()
@@ -92,9 +105,9 @@ class Server(QMainWindow, windowserver.Ui_MainWindow):
 
             else:
                 connection.send(b'please go out!')
-                count-=1
-                self.tellOthers(connection.fileno(), "New chat room people number " + str(count))
-                print(count)
+                self.count -= 1
+                self.tellOthers(connection.fileno(), "New chat room people number " + str(self.count))
+                print(self.count)
                 print("Someone leave! Chat room")
                 connection.close()
         except:
@@ -109,10 +122,7 @@ class Server(QMainWindow, windowserver.Ui_MainWindow):
                 except:
                     pass
 
-
     def subThreadIn(self, myconnection, myname,  connNumber):
-        global count
-        global id
         self.mylist.append(myconnection)
 
         while True:
@@ -125,10 +135,11 @@ class Server(QMainWindow, windowserver.Ui_MainWindow):
                     pass
 
             except (OSError, ConnectionResetError):
-                count -= 1
-                self.tellOthers(connNumber, "New chat room people number " + str(count))
-                print(count)
+                self.count -= 1
+                self.tellOthers(connNumber, "New chat room people number " + str(self.count))
                 print("Someone leave! Chat room")
+                print("Chat room people number: ", self.count)
+
                 try:
                     self.mylist.remove(myconnection)
 
@@ -137,32 +148,35 @@ class Server(QMainWindow, windowserver.Ui_MainWindow):
 
                 myconnection.close()
                 return
+
     def Input(self):
         dbChatRoom = DataBaseChatRoom()
-        dbChatRoom.Initdatabase()
-        dbChatRoom.colseClient()
+        #dbChatRoom.colseClient()
         user = self.lineEdit.text()
         print(user)
         psw = self.lineEdit_2.text()
-        dbChatRoom.insertUser(user,psw)
+        dbChatRoom.insertUser(user, psw)
+        self.lineEdit.setText('')
+        self.lineEdit_2.setText('')
+
+    def loopCheckConnect(self): #by ChenPo
+        while True:
+            print("wait for somebody...\n")
+            self.checkConnection()
+            print("somebody in!!\n")
+
+    def DelUser(self):  #by ChenPo, not done yet
+        dbChatRoom = DataBaseChatRoom()
 
 
-
-
-
-def main():
-    #s = Server('140.138.145.9', 5550)#by Ping
-    s = Server('140.138.145.16', 5550)#by ChenPo
-    global count
-    global id
-    id = list()
-    count = 0
-    while True:
-        s.checkConnection()
-
-if __name__ == "__main__":
+def main():  # by ChenPo
     app = QApplication(sys.argv)
-    MainWindow = Server('140.138.145.16', 5550)
+    MainWindow = Server('localhost', 5550)
+    th1 = threading.Thread(target=MainWindow.loopCheckConnect)
+    th1.start()
     MainWindow.show()
     sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
     main()
